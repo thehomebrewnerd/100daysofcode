110618b5b9cc08c669de7c4e396d5d5262da63b5
NateParsons <Nathan.Parsons@ab-inbev.com>
2018-01-06 10:32:49 -0600

    Initial django blog commit

---
+++ code100blog/blogs/models.py
@@ -0,0 +1,15 @@
+# blogs/models.py
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
