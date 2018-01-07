e1b73524d65f0b7e60571cc40968a1377efe867a
NateParsons <Nathan.Parsons@ab-inbev.com>
2018-01-07 14:57:05 -0600

    R1D4

--- code100blog/blogs/models.py
+++ code100blog/blogs/models.py
@@ -1,5 +1,6 @@
 # blogs/models.py
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -9,7 +10,13 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    #text = models.TextField()
+    progress = models.TextField()
+    thoughts = models.TextField()
+    
+
+    def get_absolute_url(self):
+        return reverse('home')
 
     def __str__(self):
         return self.title
