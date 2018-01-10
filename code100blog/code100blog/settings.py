9b1405d2a070b3f48fc99f3b41c63a9cf5c26e3c
NateParsons <Nathan.Parsons@ab-inbev.com>
2018-01-10 17:06:39 -0600

    R1D7-PostgreSQL day

--- code100blog/code100blog/settings.py
+++ code100blog/code100blog/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'code100blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'code100_db',
+        'USER': 'django-user',
+        'PASSWORD': 'code100days',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
